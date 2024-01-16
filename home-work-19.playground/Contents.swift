a7cb3ad62d42effef1e9039d1a22517ccbb93b62
sbaranchuk <sbaranchuk@protonmail.com>
2024-01-16 21:25:16 +0300

    Created a session loading method

--- home-work-19.playground/Contents.swift
+++ home-work-19.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func getData(urlRequaest: String) {
+    let urlRequaest = URL(string: urlRequaest)
+    guard let url = urlRequaest else { return }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if error != nil {
+
+        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            guard let tada = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+        }
+    }.resume()
+}
