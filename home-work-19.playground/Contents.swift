24df560b4c68c5658800d129595b119598b72aa8
Admin <admin@MacBook-Pro-Admin.local>
2024-02-03 22:06:15 +0300

    Added response code output to the console

--- home-work-19.playground/Contents.swift
+++ home-work-19.playground/Contents.swift
@@ -7,11 +7,12 @@ func getData(urlRequaest: String) {
         if error != nil {
             print("Error: \(String(describing: error?.localizedDescription ?? "could not retrieve description"))")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print("Server response code: \(response.statusCode)")
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
         }
     }.resume()
 }
 
-getData(urlRequaest: "https://theaxolotlapi.netlify.ap")
+getData(urlRequaest: "https://theaxolotlapi.netlify.app")
 
