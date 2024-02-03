590bbe3f3958d4e3dcf942ed50c7a21f7070c944
Admin <admin@MacBook-Pro-Admin.local>
2024-02-03 22:02:24 +0300

    Added error output

--- home-work-19.playground/Contents.swift
+++ home-work-19.playground/Contents.swift
@@ -5,10 +5,13 @@ func getData(urlRequaest: String) {
     guard let url = urlRequaest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
-
+            print("Error: \(String(describing: error?.localizedDescription ?? "could not retrieve description"))")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-            guard let tada = data else { return }
+            guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
         }
     }.resume()
 }
+
+getData(urlRequaest: "https://theaxolotlapi.netlify.ap")
+
