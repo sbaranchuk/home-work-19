85720f305c324ce264128f385d2d4bb7943d22a2
Admin <admin@MacBook-Pro-Admin.local>
2024-02-08 21:37:17 +0300

    Add func md5Hash, collected url from URLComponents

--- home-work-19.playground/Contents.swift
+++ home-work-19.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CryptoKit
 
 func getData(urlRequaest: String) {
     let urlRequaest = URL(string: urlRequaest)
@@ -15,5 +16,19 @@ func getData(urlRequaest: String) {
     }.resume()
 }
 
-getData(urlRequaest: "https://theaxolotlapi.netlify.app")
+func md5Hash(_ source: String) -> String {
+    return Insecure.MD5.hash(data: source.data(using: .utf8) ?? Data()).map { String(format: "%02hhx", $0) }.joined()
+}
 
+let ts = "1"
+let privateKey = "ec640fc9432981da7b3ed0ce5214d8ea006331a2"
+let publicKey = "b8b141d192b3f14b8217d4aa8aa3f17e"
+let hash = md5Hash(ts + privateKey + publicKey)
+
+var urlComponents = URLComponents(string: "https://gateway.marvel.com:443/v1/public/characters")
+let queryItemTs = URLQueryItem(name: "ts", value: "1")
+let queryItemApikey = URLQueryItem(name: "apikey", value: "b8b141d192b3f14b8217d4aa8aa3f17e")
+let queryItemHash = URLQueryItem(name: "hash", value: hash)
+urlComponents?.queryItems = [queryItemTs, queryItemApikey, queryItemHash]
+
+getData(urlRequaest: urlComponents?.url?.absoluteString ?? "")
